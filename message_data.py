d75d580dbc1be2ee6fd675b6c0d97be6ca4b0bd6
unknown <othman.alkhatib.en@gmail.com>
2022-04-18 01:38:41 +0300

    commit

--- message_data.py
+++ message_data.py
@@ -10,4 +10,4 @@ class ReminderData:
         return "Message: {0}; At Time: {1}".format(self.message, self.time.strftime('%d/%m/%Y %H:%M'))
 
     def should_be_fired(self):
-        return self.fired is False and datetime.datetime.now() >= self.time
+        return self.fired is False and datetime.datetime.today() >= self.time
